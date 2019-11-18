f7fe8f42445ba55d327504ad92dbb5c12d38714a
Lavínia Paganini <laviniapaganini@gmail.com>
2019-11-18 18:37:54 -0300

    Add files via upload

---
+++ dockerRode.js
@@ -0,0 +1,18 @@
+var Docker = require('../lib/docker');
+var fs     = require('fs');
+
+var socket = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
+var stats  = fs.statSync(socket);
+
+if (!stats.isSocket()) {
+  throw new Error('Are you sure the docker is running?');
+}
+
+var docker = new Docker({ socketPath: socket });
+
+docker.listContainers({all: true}, function(err, containers) {
+  console.log('ALL: ' + containers.length);
+  containers.forEach(container => {
+    console.log(container.stats());
+  });
+});
