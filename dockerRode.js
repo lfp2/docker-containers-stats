5b0cec75186d8348c732f44d49d2a9869a0b06da
marciodeaquino <masf@cin.ufpe.br>
2019-12-02 18:13:47 -0300

    interval to send data added

--- dockerRode.js
+++ dockerRode.js
@@ -15,37 +15,36 @@ var db = admin.database();
 var socket = process.env.DOCKER_SOCKET || '/var/run/docker.sock';
 var stats = fs.statSync(socket);
 
-// console.log(stats);
 if (!stats.isSocket()) {
     throw new Error('Are you sure the docker is running?');
 }
 
 var docker = new Docker({ socketPath: socket });
-// console.log(docker);
 
+let sendDataToFirebase = () => {
+    docker.listContainers({ all: false }, function(err, containers) {
+        console.log('ALL: ' + containers.length);
+        containers.forEach(containerInfo => {
+            var container = docker.getContainer(containerInfo['Id']);
+            container.stats({ stream: false }, function(err, stream) {
+                if (err) {
+                    return logger.error(err.message);
+                }
+                var ref = db.ref("docker-infos/containers/" + stream.name);
 
-docker.listContainers({ all: false }, function(err, containers) {
-    console.log('ALL: ' + containers.length);
-    containers.forEach(containerInfo => {
-        var container = docker.getContainer(containerInfo['Id']);
-        container.stats({ stream: false }, function(err, stream) {
-            if (err) {
-                return logger.error(err.message);
-            }
-            // console.log(JSON.stringify(stream, null, '\t'));
-            // console.log(JSON.stringify(stream.name, null, '\t'));
-            console.log(stream.name);
-            console.log(stream.read);
-            var ref = db.ref("docker-infos/containers/" + stream.name);
-
-            let child = JSON.stringify(stream.read);
-            let dot = child.indexOf('.');
-            child = child.substring(1, dot);
-            console.log(child);
-            var containersRef = ref.child(child);
-
-            containersRef.set(stream);
+                let child = JSON.stringify(stream.read);
+                let dot = child.indexOf('.');
+                child = child.substring(1, dot);
+                var containersRef = ref.child(child);
+                containersRef.set(stream);
+            });
+            // containerLogs(container);
         });
-        // containerLogs(container);
     });
-});
+    console.log("Dados enviados, em 30s serão enviados de novo");
+}
+
+sendDataToFirebase();
+setInterval(function() {
+    sendDataToFirebase();
+}, 30000);
